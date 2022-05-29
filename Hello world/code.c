b6ad0b8ba126a67c4f065d91fd0f7f472978b8ae
phuong-ta <phuong-ta@users.noreply.github.com>
2022-05-29 21:40:34 +0300

    add all files

---
+++ Hello world/code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+main function 
+*/
+int main(){
+    
+    // print hello world
+    printf ("Hello, World\n"); // /n add a non-visual character within a string
+    printf ("Hello\t World"); // /t equivalent to the tab key and will insert spaces within a string
+}
